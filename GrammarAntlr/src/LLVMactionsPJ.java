d3241541f212af4043df0a21a8e53cc81f41f68b
kuba <kdemianowski@gmail.com>
2015-04-26 17:49:47 +0200

    Drobne zmiany

--- GrammarAntlr/src/LLVMactionsPJ.java
+++ GrammarAntlr/src/LLVMactionsPJ.java
@@ -1,5 +1,114 @@
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import java.util.HashMap;
+
 /**
  * Created by kuba on 26.04.15.
  */
-public class LLVMactionsPJ {
+public class LLVMactionsPJ extends ProstyJezykBaseListener{
+
+    HashMap<String, String> memory = new HashMap<String, String>();
+    int value;
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterProg(@NotNull ProstyJezykParser.ProgContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitProg(@NotNull ProstyJezykParser.ProgContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterAssign_action(@NotNull ProstyJezykParser.Assign_actionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitAssign_action(@NotNull ProstyJezykParser.Assign_actionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterPrint_action(@NotNull ProstyJezykParser.Print_actionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitPrint_action(@NotNull ProstyJezykParser.Print_actionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterAssign(@NotNull ProstyJezykParser.AssignContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitAssign(@NotNull ProstyJezykParser.AssignContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterPrint(@NotNull ProstyJezykParser.PrintContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitPrint(@NotNull ProstyJezykParser.PrintContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterValue(@NotNull ProstyJezykParser.ValueContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitValue(@NotNull ProstyJezykParser.ValueContext ctx) { }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterEveryRule(@NotNull ParserRuleContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitEveryRule(@NotNull ParserRuleContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void visitTerminal(@NotNull TerminalNode node) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void visitErrorNode(@NotNull ErrorNode node) { }
+
 }
