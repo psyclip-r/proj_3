618c4db4ad0631f596e51a9f9fda1efd0099d474
kuba <kdemianowski@gmail.com>
2015-04-26 17:45:40 +0200

    Drobne zmiany

--- GrammarAntlr/src/MainPJ.java
+++ GrammarAntlr/src/MainPJ.java
@@ -1,6 +1,27 @@
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
 /**
  * Created by kuba on 26.04.15.
  */
 public class MainPJ {
+    public static void main(String[] args) throws Exception {
+        ANTLRFileStream input = new ANTLRFileStream(args[0]);
+        ProstyJezykLexer lexer = new ProstyJezykLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        ProstyJezykParser parser = new ProstyJezykParser(tokens);
+
+        ParseTree tree = parser.prog();
+
+        //System.out.println(tree.toStringTree(parser));
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+
+        walker.walk(new LLVMactions(), tree);
+
+    }
 
 }
