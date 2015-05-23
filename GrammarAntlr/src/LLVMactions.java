7fbcd2b9041a5844b4220049e8a8f870ce13fdce
kuba <kdemianowski@gmail.com>
2015-05-23 20:08:39 +0200

    obsluga real i int tak ladnie w kodzie o jaaaa

--- GrammarAntlr/src/LLVMactions.java
+++ GrammarAntlr/src/LLVMactions.java
@@ -6,7 +6,10 @@ import java.util.HashMap;
  * Created by kuba on 26.04.15.
  */
 public class LLVMactions extends ProstyJezykBaseListener{
-    HashMap<String, Integer> memory = new HashMap<String, Integer>();
+    HashMap<String, Integer> int_memory = new HashMap<String, Integer>();
+    HashMap<String, Double> real_memory = new HashMap<String, Double>();
+    HashMap<String, String> string_memory = new HashMap<String, String>();
+
     Integer value;
 
     @Override public void enterProg(@NotNull ProstyJezykParser.ProgContext ctx) {
@@ -19,16 +22,54 @@ public class LLVMactions extends ProstyJezykBaseListener{
 
     @Override public void exitPrint_action(@NotNull ProstyJezykParser.Print_actionContext ctx) {
         //System.out.println(value);
-        LLVMGenerator.printInteger(value);
+
+        // LLVMGenerator.printInteger(value);
+        if(ctx.value().INT() != null){
+            Integer tmp = Integer.valueOf( ctx.value().INT().getText() );
+            LLVMGenerator.printInteger(tmp);
+        }
+        if(ctx.value().REAL() != null){
+            //not yet implemented
+        }
+        if(ctx.value().REAL() != null){
+            //not yet implemented
+        }
+
+        if(ctx.value().NAME() != null){
+            if( int_memory.get(ctx.value().NAME().getText()) != null ){
+                LLVMGenerator.printInteger(int_memory.get(ctx.value().NAME().getText()));
+            }
+            if( real_memory.get(ctx.value().NAME().getText()) != null ){
+                //not yet implemented
+            }
+            if( string_memory.get(ctx.value().NAME().getText()) != null ){
+                // not yet implemented
+            }
+
+        }
+
     }
 
     // jak przypiszemy wartosc do zmiennej to zapisujemy
     // ja do tablicy pod okreslona nazwa
     @Override public void exitEnter_assign(@NotNull ProstyJezykParser.Enter_assignContext ctx) {
-        Integer tmp = Integer.valueOf( ctx.value().INT().getText() );
-        // do pamieci wkladamy zmienna za pomoca tablicy klucz-wartosc, np x=3 => klucz x, wartosc 3 => x=
-        memory.put(ctx.NAME().getText(), tmp);
-        //System.out.println(ctx.VARIABLE().getText() + " = " + tmp);
+        if(ctx.var_type().getText().equalsIgnoreCase("int")){
+            Integer tmp = Integer.valueOf( ctx.value().INT().getText() );
+            int_memory.put(ctx.NAME().getText(), tmp);
+        }
+        if(ctx.var_type().getText().equalsIgnoreCase("real")){
+            double tmp = Double.valueOf( ctx.value().REAL().getText() );
+            real_memory.put(ctx.NAME().getText(), tmp);
+        }
+        if(ctx.var_type().getText().equalsIgnoreCase("string")){
+            String tmp =  ctx.value().STRING().getText();
+            string_memory.put(ctx.NAME().getText(), tmp);
+        }
+        if(ctx.var_type().getText().equalsIgnoreCase("array")){
+            //not yet implemented
+        }
+
+
     }
 
     // wiemy co drukowac dzieki funkcji exitValue
@@ -48,7 +89,7 @@ public class LLVMactions extends ProstyJezykBaseListener{
             // zadeklarowana wczesniej, dlatego ja odczytujemy i wyswietlamy,
             // zmienna mamy w pamieci
             // przyklad kodu: "wyswietl x"
-            value = memory.get(ctx.NAME().getText());
+            value = int_memory.get(ctx.NAME().getText());
         }
         if( ctx.INT() != null ){
             // jak zmienna to INT
@@ -58,6 +99,9 @@ public class LLVMactions extends ProstyJezykBaseListener{
             Integer tmp = Integer.valueOf (ctx.INT().getText() );
             value = tmp;
         }
+
+
+
     }
 
 /*
