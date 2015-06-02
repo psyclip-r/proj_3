8fe8aa90e8cdb262e4d8b1d0cf0bb28c167440d0
kuba <kdemianowski@gmail.com>
2015-06-02 14:59:44 +0200

    Usuniecie nieuzywanego kodu

--- GrammarAntlr/src/LLVMGenerator.java
+++ GrammarAntlr/src/LLVMGenerator.java
@@ -97,14 +97,6 @@ class LLVMGenerator{
         br++;
     }
 
-    /*
-    static void printfInt(String id){
-        content += "%"+ register +" = load i32* %"+id+"\n";
-        register++;
-        content += "%"+ register +" = call i32 (i8*, ...)* @printf(i8* getelementptr inbounds ([4 x i8]* @strpi, i32 0, i32 0), i32 %"+(register -1)+")\n";
-        register++;
-    }
-    */
 
     static void printfInt(String id, boolean main, boolean isGlobal){
         //System.out.println(id + " is global? " + isGlobal);
@@ -134,10 +126,6 @@ class LLVMGenerator{
         register++;
     }
 
-    static void declareInt(String id){
-        content += "%"+id+" = alloca i32\n";
-        // System.out.println("Stara funkcja deklarowania!!! : " + id);
-    }
 
     static void declareInt(String id, boolean main){
         // System.out.println("declareInt: Deklaruje zmienna: " + id + " czy globalna: " + main);
@@ -193,9 +181,6 @@ class LLVMGenerator{
         content += "%"+id+" = alloca double\n";
     }
 
-    static void assignInt(String id, String value){
-        content += "store i32 "+value+", i32* %"+id+"\n";
-    }
 
     static void assignInt(String id, String value, boolean main, boolean isGlobal){
         String globalOrLocal;
@@ -225,28 +210,6 @@ class LLVMGenerator{
         }else{
             fun += "store double "+value+", double* "+globalOrLocal+id+"\n";
         }
-    }
-
-    static void assignDouble(String id, String value){
-        content += "store double "+value+", double* %"+id+"\n";
-    }
-
-    static void load_i32(String id){
-        content += "%"+register+" = load i32* %"+id+"\n";
-        register++;
-    }
-
-    static void load_double(String id){
-        content += "%"+register+" = load double* %"+id+"\n";
-        register++;
-    }
-
-
-    static void addIntVar(String val1, String var){
-        content += "%"+ register +" = load i32* "+var+"\n";
-        register++;
-        content += "%"+ register +" = add i32 "+val1+", "+(register-1)+"\n";
-        register++;
     }
 
     static void addInt(String val1, String val2){
@@ -503,90 +466,3 @@ class LLVMGenerator{
     }
 
 }
-
-
-/*
-
-public class LLVMGenerator {
-    static int num_of_number;
-    static String header = "";
-    static String mainText = "";
-    static String declaration = "";
-    static String print = "";
-    static int register;
-
-    public LLVMGenerator(){
-        num_of_number = 0;
-        register = 1;
-    }
-
-    static void addInt(String val1, String val2){
-        mainText += "%"+register+" = add i32 "+val1+", "+val2+"\n";
-        register++;
-    }
-
-    static void addFloat(String val1, String val2){
-        mainText += "%"+register+" = add i32 "+val1+", "+val2+"\n";
-        register++;
-    }
-
-    static void printf(String id){
-        mainText += "%"+register+" = load i32* %"+id+"\n";
-        register++;
-        mainText += "%"+register+" = call i32 (i8*, ...)* @printf(i8* getelementptr inbounds ([4 x i8]* @strp, i32 0, i32 0), i32 %"+(register-1)+")\n";
-        register++;
-    }
-
-    static void declare(String id){
-        mainText += "%"+id+" = alloca i32\n";
-    }
-
-    static void assign(String id, String value){
-        mainText += "store i32 "+value+", i32* %"+id+"\n";
-    }
-
-    static void load(String id){
-        mainText += "%"+register+" = load i32* %"+id+"\n";
-        register++;
-    }
-
-    /*
-    static void printInteger(int number){
-        String tmp = "%num_" + num_of_number + " = alloca i32, align 4 \n" +
-                "store i32 " + number + ", i32* %num_" + num_of_number + ", align 4 \n";
-        declaration += tmp;
-        tmp = "%l_" + num_of_number + " = load i32* %num_" + num_of_number + ", align 4 \n";
-        print += tmp;
-        tmp = "%p_" + num_of_number + " = call i32 (i8*, ...)* @printf(i8* getelementptr inbounds ([4 x i8]* @.str, i32 0, i32 0), i32 %l_" + num_of_number + ") \n";
-        print += tmp;
-        num_of_number++;
-    }
-    */
-/*
-    // na poczatku musimy ustawic pare rzeczy zawsze tak samo
-    static void enterProg(){
-        String text = "@.str = private unnamed_addr constant [4 x i8] c\"%d\\0A\\00\", align 1   \n" +
-                        "define i32 @main() #0 { \n";
-        System.out.println(text);
-    }
-
-    // na koneic ustawiamy pare rzeczy zawsze tak samo
-    static void exitProg(){
-        //System.out.println(declaration);
-        //System.out.println(print);
-
-
-        String text = "ret i32 0 \n} \n" +
-                "declare i32 @printf(i8*, ...) #1\n";
-        System.out.println(text);
-    }
-
-    static void print(String text){
-
-    }
-
-
-
-
-}
-*/
